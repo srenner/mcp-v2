6354264e49467ca8f55826ba4501a99fe583c3a3
Shawn Renner <shawn.renner@gmail.com>
2021-02-01 11:48:49 -0600

    test migrations

--- mcp/mcp/Server/Models/ApplicationUser.cs
+++ mcp/mcp/Server/Models/ApplicationUser.cs
@@ -8,5 +8,12 @@ namespace mcp.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        // TODO make this unique but optional, effectively used as a vanity URL
+        //public string Username { get; set; }
+        
+        /// <summary>
+        /// e.g. "Tom" or whatever
+        /// </summary>
+        public string DisplayName { get; set; }
     }
 }
