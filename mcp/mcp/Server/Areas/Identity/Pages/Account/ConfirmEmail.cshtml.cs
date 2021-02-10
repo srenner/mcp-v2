cdf3b5a4c9e2aef95bb0e320911961e5c9f91558
Shawn Renner <shawn.renner@gmail.com>
2021-02-10 13:14:25 -0600

    todo

--- mcp/mcp/Server/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ mcp/mcp/Server/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -41,6 +41,7 @@ namespace mcp.Server.Areas.Identity.Pages.Account
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
             StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            //TODO copy seed data to user profile
             return Page();
         }
     }
