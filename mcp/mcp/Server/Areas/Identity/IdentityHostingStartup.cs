36fa1e785892ca011af3da9eaae4147f4d96e929
Shawn Renner <shawn.renner@gmail.com>
2021-03-15 20:21:26 -0500

    scaffold identity pages

---
+++ mcp/mcp/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using mcp.Server.Data;
+using mcp.Server.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(mcp.Server.Areas.Identity.IdentityHostingStartup))]
+namespace mcp.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
