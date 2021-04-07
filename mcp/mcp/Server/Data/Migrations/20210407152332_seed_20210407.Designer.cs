33bd3d77e658a896a1ea1d6bca2728bea5d78045
Shawn Renner <shawn.renner@gmail.com>
2021-04-07 10:24:05 -0500

    correct tag seed data

--- mcp/mcp/Server/Data/Migrations/20210407152332_seed_20210407.Designer.cs
+++ mcp/mcp/Server/Data/Migrations/20210407152332_seed_20210407.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mcp.Server.Data;
 
 namespace mcp.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210407152332_seed_20210407")]
+    partial class seed_20210407
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ mcp/mcp/Server/Data/Migrations/20210407152332_seed_20210407.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mcp.Server.Data.Migrations
+{
+    public partial class seed_20210407 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Tag",
+                keyColumn: "TagID",
+                keyValue: 8,
+                column: "Description",
+                value: "A vehicle built for drift events");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Tag",
+                keyColumn: "TagID",
+                keyValue: 8,
+                column: "Description",
+                value: "A vehicle built for road race events");
+        }
+    }
+}
