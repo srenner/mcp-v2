ad8248faa777f059dad45b0cb2156b201b44149c
Shawn Renner <shawn.renner@gmail.com>
2021-07-06 10:42:01 -0500

    shopping list query

---
+++ mcp/mcp.Database/VehicleShoppingList.sql
@@ -0,0 +1,8 @@
+﻿declare @VehicleID int = 1
+
+select * from ProjectPart pp
+inner join Project p
+	on pp.ProjectID = p.ProjectID
+where pp.QuantityPurchased < pp.Quantity
+and p.VehicleID = @VehicleID
+and p.ProjectStatusID = 2
